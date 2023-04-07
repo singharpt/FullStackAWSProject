22009e6084299126299aa3edd06ad5828b06c191
singharpt <104125829+singharpt@users.noreply.github.com>
2023-04-06 19:35:37 -0500

    Fixed bug in front-end

--- user-interface/pages/index.tsx
+++ user-interface/pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
+import Typewriter from 'typewriter-effect';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,13 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p > Get started by typing your email address below </p>
+          <Typewriter
+            onInit={(typewriter) => {
+            typewriter.typeString('Hi! Get started by typing your email address below.')
+            .callFunction(() => {
+                console.log('String typed out!');})
+            .start();}}
+          />
         </div>
         <div>
           <TextField 
