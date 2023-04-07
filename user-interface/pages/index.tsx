15716347c0f7255215ea4ec6998f731be07878bb
singharpt <104125829+singharpt@users.noreply.github.com>
2023-04-06 19:40:18 -0500

    Added support to format code

--- user-interface/pages/index.tsx
+++ user-interface/pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import Typewriter from 'typewriter-effect';
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typewriter from "typewriter-effect";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -19,29 +19,39 @@ export default function Home() {
         <div className={styles.description}>
           <Typewriter
             onInit={(typewriter) => {
-            typewriter.typeString('Hi! Get started by typing your email address below.')
-            .callFunction(() => {
-                console.log('String typed out!');})
-            .start();}}
+              typewriter
+                .typeString(
+                  "Hi! Get started by typing your email address below."
+                )
+                .callFunction(() => {
+                  console.log("String typed out!");
+                })
+                .start();
+            }}
           />
         </div>
         <div>
-          <TextField 
-          required
-          
-          className={styles.inputfield}
-          id="outlined-basic"
-          color="secondary"
-          label="Email" 
-          placeholder='abc@xyz.com'> Email </TextField>
+          <TextField
+            required
+            className={styles.inputfield}
+            id="outlined-basic"
+            color="secondary"
+            label="Email"
+            placeholder="abc@xyz.com"
+          >
+            Email
+          </TextField>
         </div>
         <div>
-          <Button 
-          className={styles.button}
-          variant="contained"
-          color='secondary'> Hit Go </Button>
+          <Button
+            className={styles.button}
+            variant="contained"
+            color="secondary"
+          >
+            Hit Go
+          </Button>
         </div>
       </main>
     </>
-  )
+  );
 }
